63712bdc055dec5caa77a484846dc2480de09644
talafek96 <talafek@campus.technion.ac.il>
2020-11-25 18:20:03 +0200

    Initiate right rotation & a few basic funcs

--- avl_tree/AVL.h
+++ avl_tree/AVL.h
@@ -23,6 +23,40 @@ namespace DS
         /*********************************/
         std::shared_ptr<NODE> root;
 
+        // Helper function that allocates and returns a shared pointer to a new node for the tree.
+        static std::shared_ptr<NODE> newNode(KEY_TYPE key, VAL_TYPE val)
+        {
+            std::shared_ptr<NODE> node = std::make_shared<NODE>();
+            node->key = key;
+            node->val = val;
+            node->height = 0;
+            node->left = nullptr;
+            node->right = nullptr;
+            return node;
+        }
+
+        // General right and left rotations for balanced trees, return the new root of the tree.
+        std::shared_ptr<NODE>& rotateRight(const std::shared_ptr<NODE> sub_root)
+        {
+            std::shared_ptr<NODE> L_sub = sub_root->left;
+            std::shared_ptr<NODE> LR_sub = L_sub->right;
+
+            // Do the rotation:
+            L_sub->right = sub_root;
+            sub_root->left = LR_sub;
+
+            // Update the new heights:
+            L_sub->height = max(height(L_sub->left), height(L_sub->right)) + 1;
+            sub_root->height = max(height(sub_root->left), height(sub_root->right)) + 1;
+
+            return L_sub;
+        }
+
+        std::shared_ptr<NODE>& rotateLeft(const std::shared_ptr<NODE> sub_root)
+        {
+            
+        }
+
         /**********************************/
         /*         Public Section         */
         /**********************************/
@@ -60,15 +94,6 @@ namespace DS
             return node->height;
         }
 
-        static std::shared_ptr<NODE> newNode(KEY_TYPE key, VAL_TYPE val)
-        {
-            std::shared_ptr<NODE> node = std::make_shared<NODE>();
-            node->key = key;
-            node->val = val;
-            node->height = 0;
-            node->left = nullptr;
-            node->right = nullptr;
-            return node;
-        }
+
     };
 }
